168b95f2cd83732b00205e8afc77ed8edeb62abb
faraday58 <jarodriguezvera@gmail.com>
2026-09-04 21:45:48 -0600

    Add project files.

---
+++ FormSaludo.Designer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace WinFormSaludo_2027_I
+{
+    internal class FormSaludo: Form
+    {
+        private Label lbIntroNombre;
+
+        public FormSaludo()
+        {
+            this.Text = "Formulario de Saludo";
+            this.Size = new Size(500,300);
+
+
+            // Instanciando el label y sus propiedades
+            this.lbIntroNombre = new Label();
+            this.lbIntroNombre.Text = "Ingresa tu nombre:";
+            this.lbIntroNombre.Size = new Size(200, 30);
+            this.lbIntroNombre.Location = new Point(20,20);
+
+
+            this.Controls.Add(this.lbIntroNombre);
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿
+using WinFormSaludo_2027_I;
+
+Application.Run(new FormSaludo());
