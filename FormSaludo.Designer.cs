43986e701b1f58759054abb84672f17653b0986a
faraday58 <jarodriguezvera@gmail.com>
2026-09-04 21:56:34 -0600

    Add button

--- FormSaludo.Designer.cs
+++ FormSaludo.Designer.cs
@@ -7,21 +7,34 @@ namespace WinFormSaludo_2027_I
     internal class FormSaludo: Form
     {
         private Label lbIntroNombre;
+        private TextBox txtbNombre;
+        private Button btnAceptar;
 
         public FormSaludo()
         {
             this.Text = "Formulario de Saludo";
             this.Size = new Size(500,300);
-
-
             // Instanciando el label y sus propiedades
             this.lbIntroNombre = new Label();
             this.lbIntroNombre.Text = "Ingresa tu nombre:";
             this.lbIntroNombre.Size = new Size(200, 30);
             this.lbIntroNombre.Location = new Point(20,20);
 
+            // Instanciando el textbox y sus propiedades
+            this.txtbNombre = new TextBox();
+            this.txtbNombre.Size = new Size(300, 30);
+            this.txtbNombre.Location = new Point(20,70);
+
+            // Instanciando el Button y sus propiedades
+            this.btnAceptar = new Button();
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.Size = new Size(100, 40);
+            this.btnAceptar.Location = new Point(40, 95);
+
 
             this.Controls.Add(this.lbIntroNombre);
+            this.Controls.Add(this.txtbNombre);
+            this.Controls.Add(this.btnAceptar);
 
         }
 
